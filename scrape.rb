711c8b2fb74c812cebf008ac2cb67c27e3296f1e
David Garber <davidgarber@gmail.com>
2015-09-15 18:32:17 -0700

    Naviage the form.

--- scrape.rb
+++ scrape.rb
@@ -9,3 +9,10 @@ scraper.history_added = Proc.new{sleep 0.5}
 BASE_URL = 'http://sfbay.craigslist.org/'
 # ADDRESS is a constant that represents starting address of scraper
 ADDRESS = 'http://sfbay.craigslist.org/search/apa'
+
+scraper.get(ADDRESS) do search_page
+  search_form = search_page.form_with(:id => 'searchform') do |search|
+    search.query = "Garden Apartment"
+    search.min_price = 1000
+    search.max_price = 5000
+end
